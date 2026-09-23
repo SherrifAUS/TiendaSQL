0c4fe02a9c33daa7f80842345e1da470e929d119
Enrique Matias Fernández <128739815+SherrifAUS@users.noreply.github.com>
2026-09-22 23:40:20 -0300

    Add SQL queries for profitability and customer analysis
    Added SQL queries for analyzing sales and customer data.

--- pre-entrega-modulo4.sql
+++ pre-entrega-modulo4.sql
@@ -1,9 +1,5 @@
--- -----------------------------------------------------------------------------
+
 -- Consulta 1: Rentabilidad por categoría
--- Objetivo de negocio: Analizar el volumen de unidades vendidas y los ingresos 
--- totales por categoría para enfocar el presupuesto en las líneas más rentables.
--- Se define un umbral de $250 para excluir categorías de baja rotación.
--- -----------------------------------------------------------------------------
 select 
 cat.descripcion,
 sum(v.cantidad) as Unidades_Vendidas,
@@ -14,3 +10,7 @@ inner join categorias as cat on p.idCategoria = cat.idCategoria
 group by cat.descripcion
 having sum(v.cantidad * p.precio) > 250.00
 order by Ingreso_Total desc;
+
+--Consulta 2: Clientes sin compras -usando LEFT JOIN
+
+--Consulta 3: Top de compras por cliente
