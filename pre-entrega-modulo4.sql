aed2409a7466972a3a0641894be58f7ac3649171
Enrique Matias Fernández <128739815+SherrifAUS@users.noreply.github.com>
2026-09-23 19:29:45 -0300

    Add query for clients without purchases
    Added a query to retrieve clients without purchases using LEFT JOIN.

--- pre-entrega-modulo4.sql
+++ pre-entrega-modulo4.sql
@@ -1,4 +1,3 @@
-
 -- Consulta 1: Rentabilidad por categoría
 select 
 cat.descripcion,
@@ -12,5 +11,13 @@ having sum(v.cantidad * p.precio) > 250.00
 order by Ingreso_Total desc;
 
 --Consulta 2: Clientes sin compras -usando LEFT JOIN
+select 
+c.idCliente,
+c.nombreCliente,
+c.email,
+coalesce(count(v.idVenta), 0) as total_compras
+from clientes c
+left join ventas as v on c.idcliente = v.idcliente
+group by c.idcliente;
 
 --Consulta 3: Top de compras por cliente
