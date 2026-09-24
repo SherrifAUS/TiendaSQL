69853a5b9e41d2d1ea404028a9341cd204b010e9
Enrique Matias Fernández <128739815+SherrifAUS@users.noreply.github.com>
2026-09-23 21:14:56 -0300

    Refactor SQL queries for client purchases

--- pre-entrega-modulo4.sql
+++ pre-entrega-modulo4.sql
@@ -11,13 +11,21 @@ having sum(v.cantidad * p.precio) > 250.00
 order by Ingreso_Total desc;
 
 --Consulta 2: Clientes sin compras -usando LEFT JOIN
-select 
-c.idCliente,
-c.nombreCliente,
-c.email,
-coalesce(count(v.idVenta), 0) as total_compras
-from clientes c
-left join ventas as v on c.idcliente = v.idcliente
-group by c.idcliente;
+select
+cl.idCliente,
+cl.nombreCliente
+from clientes cl
+left join ventas as v on cl.idcliente = v.idcliente
+where v.idventa is null;
 
 --Consulta 3: Top de compras por cliente
+select 
+cl.nombreCliente,
+p.descripcion,
+count(*) as cantidad_compras,
+max(v.fechaventa) as maxima_fecha
+from ventas as v
+inner join clientes cl on v.idcliente = cl.idcliente
+inner join productos p on v.idproducto = p.idproducto
+group by cl.nombreCliente, p.descripcion
+order by cl.nombreCliente, cantidad_compras desc;
